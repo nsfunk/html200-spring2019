42b888f2881dd1b4e125c0cffa9f8981f747431a
Natalie Funk <nsfunk@gmail.com>
2019-05-08 16:48:17 -0700

    lesson 04 assignment 01 with extra challenge website

---
+++ lesson04/demo.js
@@ -0,0 +1,21 @@
+let input = prompt('Enter a number:');
+let result = 0;
+
+function squareIt(input, input){
+  return input * input;
+}
+
+result = Number(input) * Number(input);
+
+alert(result);
+
+
+function multiplier() {
+let inputOne = prompt('Enter first number');
+let inputTwo = prompt('Enter second number');
+let result = 0;
+
+result = Number(inputOne) * Number(inputTwo);
+
+alert(result);
+}
