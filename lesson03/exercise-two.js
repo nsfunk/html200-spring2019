52fb7f2b4c830fb582a55fbf1cecece115da6314
Natalie Funk <nsfunk@gmail.com>
2019-05-08 13:47:48 -0700

    finished assignment02 of lesson03

---
+++ lesson03/exercise-two.js
@@ -0,0 +1,6 @@
+let panagramPieces = ['the', 'quick', 'brown', 'fox', 'jumps', 'over', 'the', 'lazy', 'dog'];
+let panagramString = panagramPieces.join(' ');
+
+console.log(panagramString + '.');
+
+console.log(panagramPieces[2]);
