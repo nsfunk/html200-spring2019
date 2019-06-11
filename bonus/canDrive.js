645d1975a66bee872720570dad34092abb0e2ee4
Natalie Funk <nsfunk@gmail.com>
2019-06-11 10:52:01 -0700

    adding folder for bonus assignments

---
+++ bonus/canDrive.js
@@ -0,0 +1,13 @@
+const firstName = 'Natalie';
+const lastName = 'Funk';
+const age = 5;
+const legalAge = 16;
+
+if (age >= legalAge) {
+  console.log(firstName + " " + lastName + " " + "can drive");
+  console.log('They have been driving for' + " " + Number(age - legalAge) + " " + 'years!');
+} else {
+  for (let i = 5; i >= 0; i--) {
+    console.log(i);
+  }
+}
