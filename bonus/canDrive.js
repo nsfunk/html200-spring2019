ceac5417c5d11a3f53e170cf1fff977d634a0d1c
Natalie Funk <nsfunk@gmail.com>
2019-06-11 10:53:50 -0700

    created canDrive.js bonus assignment

--- bonus/canDrive.js
+++ bonus/canDrive.js
@@ -5,7 +5,7 @@ const legalAge = 16;
 
 if (age >= legalAge) {
   console.log(firstName + " " + lastName + " " + "can drive");
-  console.log('They have been driving for' + " " + Number(age - legalAge) + " " + 'years!');
+  console.log("They have been driving for" + " " + Number(age - legalAge) + " " + "years!");
 } else {
   for (let i = 5; i >= 0; i--) {
     console.log(i);
